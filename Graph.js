fd34898a1ad59c812eee3ee25e5ec19e1ea81ef2
= <alexander.birhalaconstantinescu@amadeus.com>
2018-09-02 22:54:43 +0200

    Backup old files

--- Graph.js
+++ Graph.js
@@ -2,45 +2,95 @@ function Tree() {
   this.vertices = [];
 }
 
-Tree.prototype.dfsWalk = function() {
-  // body...
+Tree.prototype.findRoot = function() {
+
 };
 
-Tree.prototype.preOrderWalk = function() {
-  // body...
+Tree.prototype.preOrder = function() {
+
 };
 
-Tree.prototype.postOrderWalk = function() {
-  // body...
+Tree.prototype.dfs = function() {
+  var visitedVertices = [], stack = [];
+  var vertex = this.findRoot();
+  var isVertexUnvisited = function(id) {return visitedVertices.indexOf(id) === -1;};
+  return this.dfsWalk(visitedVertices, stack, vertex, isVertexUnvisited);
 };
 
-Tree.prototype.buildPath = function() {
-  // body...
+// DFS walk implementation with nearest neighbour visit
+Tree.prototype.dfsWalk = function(visitedVertices, stack, vertex, isVertexUnvisited) {
+  if (isVertexUnvisited(vertex)) {
+    visitedVertices.push(vertex.id);
+    stack.push(vertex.id);
+  }
+
+  var minimumCost = Infinity, nextVertex;
+
+  for (var i = 0; i < vertex.edges.length; i++) {
+    if (vertex.edges[i].cost < minimumCost && isVertexUnvisited(vertex.edges[i].id)) {
+      minimumCost = vertex.edges[i].cost;
+      nextVertex = vertex.edges[i].id;
+    }
+  }
+
+  if (isVertexUnvisited(vertex.edges[i].id) && nextVertex !== undefined) {
+    this.dfsWalk(visitedVertices, nextVertex);
+  } else if (stack.length > 0) {
+    stack.pop(vertex);
+    this.dfsWalk(visitedVertices, stack[stack.length-1]);
+  } else {
+    return visitedVertices;
+  }
 };
 
 Tree.prototype.computeWeight = function() {
   var weight;
-  this.vertices.forEach(function(vertice) {
-    vertices.edges.forEach(function(edge) {
+  this.vertices.forEach(function(vertex) {
+    vertex.edges.forEach(function(edge) {
       weight += edge.cost;
     });
   });
   return weight;
 };
 
-Tree.prototype.addVertice = function(vertice) {
-  var foundVertice = this.vertices.filter(function(searchVertice){ return searchVertice.id === vertice.id; });
-  if (foundVertice.length <= 0) {
-    this.vertices.push(vertice);
-    return vertice;
+Tree.prototype.addVertex = function(vertex) {
+  var foundVertex = this.vertices.filter(function(searchVertex){ return searchVertex.id === vertex.id; });
+  if (foundVertex.length <= 0) {
+    this.vertices.push(vertex);
+    return vertex;
   }
-  return foundVertice[0];
+  return foundVertex[0];
+};
+
+Tree.prototype.removeVertex = function(vertex) {
+  if (this.vertices.indexOf(vertex) >= 0) {
+    this.vertices.splice(foundVertexIdx, 1);
+  }
+};
+
+Tree.prototype.hasEulerianCycle = function(){
+  return this.vertices.every(function(vertex) {
+    return (this.getVertexDegree(vertex) % 2) === 0;
+  }.bind(this));
+};
+
+Tree.prototype.getVertexDegree = function(vertex) {
+  return this.getOutDegree(vertex) + 1; // Add plus one for the in degree, as we are dealing with a tree.
+};
+
+
+Tree.prototype.getOutDegree = function(vertex) {
+  var foundVertex = this.vertices.filter(function(searchVertex){ return searchVertex.id === vertex.id; });
+  if (foundVertex.length > 0) {
+    return foundVertex[0].edges.length;
+  }
+  throw new Error("Vertex: " + vertex + " is not included in the tree");
 };
 
 
 // ----------------------------------------------
 
-function Vertice(id) {
+function Vertex(id) {
   this.id = id;
   this.edges = [];
 }
@@ -50,6 +100,8 @@ Vertice.prototype.addEdge = function(toId, cost) {
 };
 
 Vertice.prototype.removeEdge = function(id) {
-  var removalEdge = this.edges.filter(function(edge){return edge.to == id; });
-  this.edges.splice(this.edges.indexOf(removalEdge),1);
+  var removalEdge = this.edges.filter(function(edge){return edge.to === id; });
+  if (removalEdge > 0) {
+    this.edges.splice(this.edges.indexOf(removalEdge),1);
+  }
 };
